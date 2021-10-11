787dd2f2ca08fc2da213a56754d3d6918ffd9ddf
Denis Khaliullin <deniskhaliullin@gmail.com>
2021-10-11 09:26:50 +0500

    feat: add mapbox in contact

--- museum/src/index.js
+++ museum/src/index.js
@@ -144,6 +144,40 @@ function initComparisons() {
   }
 }
 
+//map-box
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGVuaXNraGFsaXVsbGluIiwiYSI6ImNrdW0zZzNueTFzMjQycG15MjkzOTVmcHQifQ.WAma_bRwWN5FjRzMWuuAug';
+const map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/light-v10',
+center: [2.3364, 48.86091],
+zoom: 15.5
+});
+
+// Create a default Marker and add it to the map.
+const marker1 = new mapboxgl.Marker({ color: 'black'})
+.setLngLat([2.3333, 48.8602])
+.addTo(map);
+ 
+// Create a default Marker, colored black, rotated 45 degrees.
+const marker2 = new mapboxgl.Marker({ color: 'black'})
+.setLngLat([2.3397, 48.8607])
+.addTo(map);
+
+const marker3 = new mapboxgl.Marker({ color: 'black'})
+.setLngLat([2.3364, 48.86091])
+.addTo(map);
+
+const marker4 = new mapboxgl.Marker({ color: 'black'})
+.setLngLat([2.3330, 48.8619])
+.addTo(map);
+
+const marker5 = new mapboxgl.Marker({ color: 'black'})
+.setLngLat([2.3365, 48.8625])
+.addTo(map);
+
+map.addControl(new mapboxgl.NavigationControl());
+
 initComparisons();
 buy();
 
