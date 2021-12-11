82537bc7bf31b4f75aa46ce7f962c7d7e923b853
Denis Khaliullin <deniskhaliullin@gmail.com>
2021-12-11 21:49:40 +0500

    fix: clean main branch

--- museum/src/js/gallery.js
+++
@@ -1,7 +0,0 @@
-function newPicture () {
-    const pictureInnerContainer = document.querySelector('.picture-inner__wrapper');
-
-const img = `<img class="gallery-img" src="../museum/assets/img/galery/galery1.jpg" alt="galery1">`;
-pictureInnerContainer.innerHTML = img;
-
-}
